15a8f863a0e324626e7eb4f591daf89e6e2fad43
edison <edison.rave1517@gmail.com>
2020-01-17 17:04:36 -0500

    odontograma seleciconable

--- main.js
+++ main.js
@@ -1,20 +1,139 @@
-var elCanvas = document.getElementById("canvas");
-var context = elCanvas.getContext("2d");
-
-var TeethInstance = new Teeth(elCanvas, context);
 
 
+   
+
+     
 
 
-var init = 40;
-
-var middleX = elCanvas.width;
-var middleY = elCanvas.height;
-console.log("Ancho del canvas x " + middleX);
-console.log("Ancho del canvas y " + middleY);
 
 
-dividirCanvas(middleX, middleY);
-adultOdontogram(middleX, middleY);
+  /*
 
 
+      var face2 = new Konva.Arc({
+        x: stage.width() / 2,
+        y: stage.height() / 2,
+        innerRadius: 40,
+        outerRadius: 70,
+        angle: 90,
+        fill: 'red',
+        stroke: 'black',
+        rotation:90,
+        strokeWidth: 4,
+       
+      });
+
+
+      var face3 = new Konva.Arc({
+        x: stage.width() / 2,
+        y: stage.height() / 2,
+        innerRadius: 40,
+        outerRadius: 70,
+        angle: 90,
+        fill: 'red',
+        stroke: 'black',
+        rotation:180,
+        strokeWidth: 4,
+       
+      });
+
+      var face4 = new Konva.Arc({
+        x: stage.width() / 2,
+        y: stage.height() / 2,
+        innerRadius: 40,
+        outerRadius: 70,
+        angle: 90,
+        fill: 'red',
+        stroke: 'black',
+        rotation:270,
+        strokeWidth: 4,
+       
+      });
+
+      face4.on('mouseover', function() {
+        this.stroke("blue");
+        this.fill("blue");
+        layer.draw();
+      });
+
+      face4.on('mouseleave', function() {
+        this.stroke("black");
+        this.fill("red");
+
+        layer.draw();
+      });
+
+  */
+
+      class teeth
+      {
+         stage = new Konva.Stage({
+            container: 'container',
+            width: 800,
+            height: 800
+          });
+          layer = new Konva.Layer();
+          constructor(){
+             
+            
+             
+          }
+
+
+          drawTeeth(x, y){
+                // add the shape to the layer
+                var faces = [
+                    this.drawFace(0, 90, this.stage, this.layer,x, y),
+                    this.drawFace(90, 90, this.stage, this.laye,x, y),
+                    this.drawFace(180, 90, this.stage, this.laye,x, y),
+                    this.drawFace(270, 90, this.stage, this.laye,x, y)
+                ]
+               
+                for(var k=0;k<4;k++)
+                {
+                   
+                   
+                      this.layer.add(faces[k]);
+                      this.layer.draw();
+                }
+                this.layer.draw();
+                this.stage.add(this.layer);
+             
+          }
+
+          drawFace(rotation, angle,  stage, layer, x, y){
+           let face = new Konva.Arc({
+                x: x,
+                y: y,
+                innerRadius: 40,
+                outerRadius: 70,
+                angle: angle,
+                fill: 'red',
+                stroke: 'black',
+                rotation:rotation,
+                strokeWidth: 4,
+               
+              });
+
+              face.on('mouseover', function() {
+                console.log("paso por encima");
+                this.stroke("blue");
+                this.fill("blue");
+                this.draw();
+              });
+
+              face.on('mouseleave', function() {
+                console.log("paso por encima");
+                this.stroke("black");
+                this.fill("red");
+                this.draw();
+              });
+
+              return face;
+          }
+      }
+
+
+       var teeths = new teeth();
+       teeths.drawTeeth(200,200);
+       teeths.drawTeeth(400,200);
